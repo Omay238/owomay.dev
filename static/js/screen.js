e3ae6e7b90f85207a410365fc73ebdc547c99f7a
Omay238 <238maculo238@gmail.com>
2026-01-02 23:23:55 -0700

    day 2
    ~5hrs
    
    all for today was just a looot of work on styling. less than yesterday, but most input elements are styled properly

--- static/js/screen.js
+++ static/js/screen.js
@@ -1,20 +1,8 @@
 function resize() {
-  const baseW = 640;
-  const baseH = 480;
-  const scale = Math.min(window.innerWidth / baseW, window.innerHeight / baseH);
-  const body = document.body;
+  let scale = Math.min(window.innerWidth / 640, window.innerHeight / 480);
 
-  const scaledW = baseW * scale;
-  const scaledH = baseH * scale;
-  const left = Math.round((window.innerWidth - scaledW) / 2);
-  const top = Math.round((window.innerHeight - scaledH) / 2);
-
-  body.style.position = 'absolute';
-  body.style.left = `${left}px`;
-  body.style.top = `${top}px`;
-  body.style.transformOrigin = 'top left';
-  body.style.transform = `scale(${scale})`;
+  document.body.style.transform = `scale(${scale})`;
 }
 
-window.addEventListener('load', resize);
-window.addEventListener('resize', resize);
+window.addEventListener("load", resize);
+window.addEventListener("resize", resize);
